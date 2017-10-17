3f74e69fd113a752ff898b54839df2744cee599b
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-10-16 21:33:01 -0400

    controller methods/redirects/views--only outlined

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,2 +1,40 @@
 class EntryController <ApplicationController
+
+
+  get '/entries' do
+
+    erb :"entries/index"
+  end
+
+  get '/entries/new' do
+
+    erb :"entries/new"
+  end
+
+  post '/entries' do
+
+    redirect '/entries'
+
+  end
+
+  get '/entries/:id' do
+
+    erb :"entries/show"
+  end
+
+  get '/entries/:id/edit' do
+
+    erb :"entries/edit"
+  end
+
+  patch '/entries/:id/edit' do
+
+  end
+
+  delete '/entries/:id' do
+
+  end
+
+
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,38 @@
 class UserController < ApplicationController
+
+
+  get 'users/signup' do
+
+    erb :"users/signup"
+
+  end
+
+  post 'users/signup' do
+
+    redirect "users/#{@user.slug}"
+
+  end
+
+  get 'users/login' do
+
+    erb :"users/login"
+
+  end
+
+  post 'users/login' do
+
+    redirect "users/#{@user.slug}"
+  end
+
+  get 'users/:slug' do
+
+    erb :"users/home"
+
+  end
+
+  get 'users/logout' do
+    session.clear
+
+    redirect "users/login"
+  end
 end
